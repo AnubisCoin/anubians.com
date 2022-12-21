e717f4d4d3d6a5309a107a1aed9f7585df9d2c77
michielpost <michiel@michielpost.nl>
2022-12-21 08:48:09 +0000

    Deploying to gh-pages from @ AnubisCoin/anubians.com@995ecf97e3f0a607e29157f068cde60fed0ad620 🚀

---
+++ js/mintstatus.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/statusHub").build();
+
+connection.start().then(function () {
+    connection.invoke("AddToGroup", mintId).catch(function (err) {
+        return console.error(err.toString());
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("StatusUpdate", async function (id) {
+    if (id != mintId) {
+        console.log('Unexpected id ' + id);
+        return;
+    }
+
+    let response = await fetch('/mint/_status/' + mintId);
+    if (response.ok) {
+        document.getElementById('statusDiv').innerHTML = await response.text();
+    }
+});
+
